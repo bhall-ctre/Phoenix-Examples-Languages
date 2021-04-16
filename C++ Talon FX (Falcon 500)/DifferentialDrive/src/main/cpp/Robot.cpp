edcc718fb0c7eef72d18d317ea2c9b74fdc98b67
Benjamin Hall <bhallctre@gmail.com>
2021-04-16 15:51:56 -0400

    Finished FX examples

--- C++ Talon FX (Falcon 500)/DifferentialDrive/src/main/cpp/Robot.cpp
+++ C++ Talon FX (Falcon 500)/DifferentialDrive/src/main/cpp/Robot.cpp
@@ -32,11 +32,8 @@
 #include <string>
 
 #include "frc/WPILib.h"
-#include "frc/kinematics/DifferentialDriveOdometry.h"
-#include "frc/simulation/DifferentialDrivetrainSim.h"
-#include "frc/smartdashboard/Field2d.h"
-#include "units/units.h"
 #include "ctre/Phoenix.h"
+#include "DrivebaseSimFX.h"
 
 using namespace frc;
 
@@ -53,6 +50,10 @@ public:
 	DifferentialDrive _diffDrive{_leftFront, _rghtFront};
 
 	Joystick _joystick{0};
+
+	void SimulationPeriodic() {
+		_driveSim.Run();
+	}
 
 	void TeleopPeriodic() {
 
@@ -119,110 +120,11 @@ public:
 		*/
 		_diffDrive.SetRightSideInverted(false);
 
-		frc::SmartDashboard::PutData(wpi::StringRef("Field"), &_field);
-	}
-
-	void RobotPeriodic() {
-		// This will get the simulated sensor readings that we set
-		// in the previous article while in simulation, but will use
-		// real values on the robot itself.
-		_odometry.Update(_pigeon.GetRotation2d(),
-						units::meter_t{NativeUnitsToDistanceMeters(_leftFront.GetSelectedSensorPosition())},
-						units::meter_t{NativeUnitsToDistanceMeters(_rghtFront.GetSelectedSensorPosition())});
-		_field.SetRobotPose(_odometry.GetPose());
-	}
-
-	// Object for simulated inputs into Talon.
-	TalonFXSimCollection _leftFrontSim = _leftFront.GetSimCollection();
-	TalonFXSimCollection _rghtFrontSim = _rghtFront.GetSimCollection();
-	// Object for simulated inputs into Pigeon.
-	PigeonIMUSimCollection _pigeonSim = _pigeon.GetSimCollection();
-	
-	//These numbers are an example AndyMark Drivetrain with some additional weight.  This is a fairly light robot.
-	//Note you can utilize results from robot characterization instead of theoretical numbers.
-	//https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-characterization/introduction.html#introduction-to-robot-characterization
-	const int kCountsPerRev = 2048;  //Encoder counts per revolution of the motor shaft.
-	const double kSensorGearRatio = 1; //Gear ratio is the ratio between the *encoder* and the wheels.  On the AndyMark drivetrain, encoders mount 1:1 with the gearbox shaft.
-	const double kGearRatio = 10.71; //Switch kSensorGearRatio to this gear ratio if encoder is on the motor instead of on the gearbox.
-	const units::inch_t kWheelRadiusInches = 3_in;
-	const int k100msPerSecond = 10;
-
-	//Simulation model of the drivetrain
-	frc::sim::DifferentialDrivetrainSim _driveSim{
-		frc::DCMotor::Falcon500(2),  //2 Falcon 500s on each side of the drivetrain.
-		kGearRatio,                  //Standard AndyMark Gearing reduction.
-		2.1_kg_sq_m,                 //MOI of 2.1 kg m^2 (from CAD model).
-		26.5_kg,                     //Mass of the robot is 26.5 kg.
-		kWheelRadiusInches,          //Robot uses 3" radius (6" diameter) wheels.
-		0.546_m,                     //Distance between wheels is _ meters.
-		
-		// The standard deviations for measurement noise:
-		// x and y:          0.001 m
-		// heading:          0.001 rad
-		// l and r velocity: 0.1   m/s
-		// l and r position: 0.005 m
-		//VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005) //Uncomment this line to add measurement noise.
-	};
-
-	frc::Field2d _field;
-	// Creating my odometry object. Here,
-	// our starting pose is 5 meters along the long end of the field and in the
-	// center of the field along the short end, facing forward.
-	frc::DifferentialDriveOdometry _odometry{_pigeon.GetRotation2d()};
-
-	void SimulationPeriodic() {
-		// Set the inputs to the system. Note that we need to use
-		// the output voltage, NOT the percent output.
-		_driveSim.SetInputs(units::volt_t{_leftFrontSim.GetMotorOutputLeadVoltage()},
-							units::volt_t{-_rghtFrontSim.GetMotorOutputLeadVoltage()}); //Right side is inverted, so forward is negative voltage
-
-		// Advance the model by 20 ms. Note that if you are running this
-		// subsystem in a separate thread or have changed the nominal timestep
-		// of TimedRobot, this value needs to match it.
-		_driveSim.Update(0.02_s);
-
-		// Update all of our sensors.
-		_leftFrontSim.SetIntegratedSensorRawPosition(
-						DistanceToNativeUnits(
-						_driveSim.GetLeftPosition().to<double>()));
-		_leftFrontSim.SetIntegratedSensorVelocity(
-						VelocityToNativeUnits(
-						_driveSim.GetLeftVelocity().to<double>()));
-		_rghtFrontSim.SetIntegratedSensorRawPosition(
-						DistanceToNativeUnits(
-						-_driveSim.GetRightPosition().to<double>()));
-		_rghtFrontSim.SetIntegratedSensorVelocity(
-						VelocityToNativeUnits(
-						-_driveSim.GetRightVelocity().to<double>()));
-		_pigeonSim.SetRawHeading(_driveSim.GetHeading().Degrees().to<double>());
-
-		//Update other inputs to Talons
-		_leftFrontSim.SetBusVoltage(RobotController::GetInputVoltage());
-		_rghtFrontSim.SetBusVoltage(RobotController::GetInputVoltage());
+		frc::SmartDashboard::PutData(wpi::StringRef("Field"), &_driveSim.GetField());
 	}
 
 private:
-	int DistanceToNativeUnits(double positionMeters){
-		double wheelRotations = positionMeters/(2 * wpi::math::pi * kWheelRadiusInches.convert<units::meter>().to<double>());
-		double motorRotations = wheelRotations * kSensorGearRatio;
-		int sensorCounts = (int)(motorRotations * kCountsPerRev);
-		return sensorCounts;
-	}
-
-	int VelocityToNativeUnits(double velocityMetersPerSecond){
-		double wheelRotationsPerSecond = velocityMetersPerSecond/(2 * wpi::math::pi * kWheelRadiusInches.convert<units::meter>().to<double>());
-		double motorRotationsPerSecond = wheelRotationsPerSecond * kSensorGearRatio;
-		double motorRotationsPer100ms = motorRotationsPerSecond / k100msPerSecond;
-		int sensorCountsPer100ms = (int)(motorRotationsPer100ms * kCountsPerRev);
-		return sensorCountsPer100ms;
-	}
-
-	double NativeUnitsToDistanceMeters(double sensorCounts){
-		double motorRotations = (double)sensorCounts / kCountsPerRev;
-		double wheelRotations = motorRotations / kSensorGearRatio;
-		double positionMeters = wheelRotations * (2 * wpi::math::pi * kWheelRadiusInches.convert<units::meter>().to<double>());
-		return positionMeters;
-	}
+	DrivebaseSimFX _driveSim{_leftFront, _rghtFront, _pigeon};
 };
 
 #ifndef RUNNING_FRC_TESTS
